66e92474b7c649e0b98ef4009293aeeca4aec575
TAMDRARI yacine <yacinetamdrari5@gmail.com>
2023-11-15 17:20:45 -0500

    main to lunch the game

--- bin/main.ml
+++ bin/main.ml
@@ -3,9 +3,9 @@ open Fanorona.Arena
 let () =
   Format.open_vbox 0;
   let result =
-    Lwt_main.run (arena (pair ~w:(player_teletype W) ~b:(player_teletype B)))
+    Lwt_main.run (arena (pair ~w:(player_teletype W) ~b:(player_random B)))
   in
-  Format.printf "Game ends with %a@," pp_endplay result.endgame;
+  Format.printf "Game ends with %a@," pp_endgame result.endgame;
   Format.printf "Trace: @[<v>%a@]" pp_trace result.trace;
   Format.close_box ();
   Format.printf "@."
