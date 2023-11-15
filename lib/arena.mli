1673a22f6108b671e7b54f2858f3ab466009bfba
TAMDRARI yacine <yacinetamdrari5@gmail.com>
2023-11-15 17:20:27 -0500

    interface for arena

--- lib/arena.mli
+++ lib/arena.mli
@@ -3,16 +3,14 @@ open Engine
 type trace = move list
 
 val pp_trace : Format.formatter -> trace -> unit
-val show_trace : trace -> string
 val equal_trace : trace -> trace -> bool
 
-type endgame = Win of player | Giveup of player | Abort of player
+type endgame = Win of player | Giveup of player
 
 val equal_endgame : endgame -> endgame -> bool
+val pp_endgame : Format.formatter -> endgame -> unit
 
 type result = { trace : trace; endgame : endgame; final : board }
-
-val pp_endplay : Format.formatter -> endgame -> unit
 
 val arena :
   ?init_player:player ->
@@ -20,7 +18,6 @@ val arena :
   (player -> board -> (move option * capture option) option Lwt.t) ->
   result Lwt.t
 
-val player_giveup : board -> (move option * capture option) option Lwt.t
 val players_trace : trace -> player -> board -> move option Lwt.t
 
 val pair :
@@ -31,4 +28,9 @@ val pair :
   (move option * capture option) option Lwt.t
 
 val player_teletype :
+  player -> board -> (move option * capture option) option Lwt.t
+
+val player_giveup : board -> (move option * capture option) option Lwt.t
+
+val player_random :
   player -> board -> (move option * capture option) option Lwt.t
